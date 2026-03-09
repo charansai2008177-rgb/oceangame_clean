36b03ce76b378229e6f7a6150d7bb029168ae0f9
charansai2008177-rgb <charansai2008177@gmail.com>
2026-03-09 17:28:25 +0530

    TREE CHOPPING

--- Assets/ITEMS/TreeResource.cs
+++ Assets/ITEMS/TreeResource.cs
@@ -7,31 +7,49 @@ public class TreeResource : MonoBehaviour
     public Item woodItem;
     public int woodAmount = 3;
 
-    [Header("Prefabs")]
-    public GameObject fallingTreePrefab;
-
-    [Header("UI")]
+    [Header("References")]
+    public GameObject Player;
     public InteractionUi interactUI;
+    private Animator playerAnimator;
+    private ItemPickupHandler playerInventory;
 
     [Header("Settings")]
     public float chopTime = 3f;
     public float respawnTime = 30f;
+    public float timeBeforeDisappear = 5f;
 
     private bool playerNearby = false;
+    private float timer = 0f;
     private bool treeChopped = false;
 
-    private float timer = 0f;
-
-    private Animator playerAnimator;
-    private ItemPickupHandler playerInventory;
-
-    private MeshRenderer treeRenderer;
+    private Rigidbody rb;
     private Collider treeCollider;
+    private MeshRenderer[] treeRenderers;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
 
     void Start()
     {
-        treeRenderer = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
         treeCollider = GetComponent<Collider>();
+        // Get all renderers in case the tree has multiple parts (leaves/trunk)
+        treeRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+
+        SetupPhysics();
+    }
+
+    void SetupPhysics()
+    {
+        rb.isKinematic = true;
+        rb.useGravity = true;
+        // This forces the "weight" to the bottom so it doesn't sink as easily
+        rb.centerOfMass = new Vector3(0, -2f, 0);
+        // Prevents the tree from sliding around like ice
+        rb.linearDamping = 1f;
+        rb.angularDamping = 1f;
     }
 
     void Update()
@@ -41,65 +59,19 @@ public class TreeResource : MonoBehaviour
         if (playerNearby)
         {
             interactUI.gameObject.SetActive(true);
-        }
 
-        if (playerNearby && Input.GetKey(KeyCode.E))
-        {
-            timer += Time.deltaTime;
-
-            interactUI.UpdateFill(timer / chopTime);
-
-            if (playerAnimator != null)
-                playerAnimator.SetTrigger("chop");
-
-            if (timer >= chopTime)
+            if (Input.GetKey(KeyCode.E))
             {
-                ChopTree();
+                if (playerAnimator != null) playerAnimator.SetTrigger("chop");
+
+                timer += Time.deltaTime;
+                interactUI.UpdateFill(timer / chopTime);
+
+                if (timer >= chopTime) ChopTree();
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            ResetChop();
-        }
-    }
-
-    void ChopTree()
-    {
-        if (treeChopped) return;
-        treeChopped = true;
-
-        // 1. Hide the tree instantly
-        treeRenderer.enabled = false;
-        treeCollider.enabled = false;
-        interactUI.gameObject.SetActive(false);
-
-        // 2. Give the wood
-        if (playerInventory != null)
-        {
-            playerInventory.PickupItem(woodItem, woodAmount);
-        }
-
-
-        // 4. Start the respawn timer
-        Invoke(nameof(RespawnTree), respawnTime);
-    }
-
-    void HideTreeVisuals()
-    {
-        treeRenderer.enabled = false;
-    }
-
-    void RespawnTree()
-    {
-        // Reset everything for the next round
-        treeRenderer.enabled = true;
-        treeCollider.enabled = true;
-        treeChopped = false;
-
-        // Reset the animation to Idle
-        Animator treeAnim = GetComponent<Animator>();
-        treeAnim.Play("TreeIdle");
+        if (Input.GetKeyUp(KeyCode.E)) ResetChop();
     }
 
     void ResetChop()
@@ -108,19 +80,60 @@ public class TreeResource : MonoBehaviour
         interactUI.ResetFill();
     }
 
+    void ChopTree()
+    {
+        if (treeChopped) return;
+
+        treeChopped = true;
+        interactUI.gameObject.SetActive(false);
+
+        // 1. Activate Physics
+        rb.isKinematic = false;
+
+        // 2. Calculate Direction
+        Vector3 fallDirection = (transform.position - Player.transform.position).normalized;
+
+        // 3. APPLY FORCE AT THE TOP (Leverage)
+        // We push the top of the tree (Vector3.up * 5) so it tips over the base
+        Vector3 forcePoint = transform.position + Vector3.up * 5f;
+        rb.AddForceAtPosition(fallDirection * 1f, forcePoint, ForceMode.Impulse);
+
+        // 4. Inventory
+        if (playerInventory != null) playerInventory.PickupItem(woodItem, woodAmount);
+
+        // 5. Cleanup
+        Invoke(nameof(HideTree), timeBeforeDisappear);
+        Invoke(nameof(RespawnTree), respawnTime);
+    }
+
+    void HideTree()
+    {
+        foreach (var renderer in treeRenderers) renderer.enabled = false;
+        treeCollider.enabled = false;
+        rb.isKinematic = true;
+    }
+
+    void RespawnTree()
+    {
+        rb.isKinematic = true;
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+
+        treeChopped = false;
+        timer = 0f;
+
+        foreach (var renderer in treeRenderers) renderer.enabled = true;
+        treeCollider.enabled = true;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Axe"))
-        {
-            // Tell the tree to shake!
-            GetComponent<TreeShake>().TriggerShake();
-
-            // Optional: Play a "thwack" sound effect here
-        }
         if (other.CompareTag("Player"))
         {
             playerNearby = true;
-
             playerInventory = other.GetComponent<ItemPickupHandler>();
             playerAnimator = other.GetComponent<Animator>();
         }
